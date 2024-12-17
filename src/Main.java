3dab4238ff778d64fe7dc3166e18de89f72da8d5
VladTca <votsats@gmail.com>
2024-12-17 16:31:52 +0100

    Update greeting message in Main.java
    Replaced the "Hello, World!" message with a personalized greeting in Russian and added a statement about learning Java. This ensures a more localized and relevant output for the program.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("меня зовут Владимир");
+        System.out.println("Я учу Java");
     }
 }
